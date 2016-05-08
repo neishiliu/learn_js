0053ebd2ed051299aa327c9897bfecd6893529fa
neishiliu <liuqiang0832@foxmail.com>
2016-05-08 22:35:37 +0800

    遍历节点设置有关元素样式 事件发生时设置有关元素样式

---
+++ test6/scripts/highlightRows.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Administrator on 2016/5/8.
+ */
+function highlightRows(){
+    if (!document.getElementsByTagName) return false;
+    var rows = document.getElementsByTagName("tr");
+    for (var i=0;i<rows.length;i++) {
+        rows[i].onmouseover = function() {
+            this.style.fontWeight = "bold";
+        };
+        rows[i].onmouseout = function() {
+            this.style.fontWeight = "normal";
+        }
+    }
+}
+addLoadEvent(highlightRows);
